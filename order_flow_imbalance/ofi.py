3adc1a11d7ac93d565994cddef4f3d39dd7c943e
nicolezattarin <72666452+nicolezattarin@users.noreply.github.com>
2022-01-12 19:23:55 +0100

    minor fixes

--- order_flow_imbalance/ofi.py
+++ order_flow_imbalance/ofi.py
@@ -9,11 +9,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--data", default='../data_cleaned/time_evolution_10_levels.csv',  \
                     help="filename.", type=str)
 parser.add_argument("--maxlevel", default=10, help="Maximum level of the book to study", type=int)
-parser.add_argument("--time_delta", default=30, help="Time delta in minutes", type=float)
+parser.add_argument("--time_delta", default=1, help="Time delta in minutes", type=float)
 parser.add_argument("--acquisition_day", default='2020-04-06', \
                     help="First day of data acquisition in format YYYY-MM-DD", type=str)
 
-def main(data, maxlevel, time_delta, acquisition_day):
+def main(data, maxlevel, time_delta, acquisition_day, tick_size=1e-4):
     """
     Computes the order flow imbalance for each level of the book and 
     computes the corresponding evolution of mid price. 
@@ -40,11 +40,15 @@ def main(data, maxlevel, time_delta, acquisition_day):
     conversion = 1e9
     df['time_isoformat'] = df['time'].apply(lambda x: datetime.fromtimestamp(x/conversion))
 
+    #rescaling prices
+    for i in range(0, maxlevel):
+        df['ask_price_{}'.format(i)] = df['ask_price_{}'.format(i)]*tick_size
+        df['bid_price_{}'.format(i)] = df['bid_price_{}'.format(i)]*tick_size
+    df['mid_price'] = df['mid_price']*tick_size
 
     #computing the quantities delta V delta D
     for i in range(maxlevel):
         print('level {}'.format(i))
-
         check_bid_prices = np.diff (df['bid_price_{}'.format(i)])
         check_ask_prices = np.diff (df['ask_price_{}'.format(i)])
         delta_W = [0]
@@ -54,7 +58,6 @@ def main(data, maxlevel, time_delta, acquisition_day):
         # for future improvement: this process can be optimized by employing .diff and masks 
         with tqdm(total=len(df)) as pbar:
             for bcheck, acheck in zip(check_bid_prices, check_ask_prices):
-
                 if bcheck > 0:
                     delta_W.append(np.array(df['bid_volume_{}'.format(i)])[j])
                 elif bcheck == 0:
@@ -87,8 +90,6 @@ def main(data, maxlevel, time_delta, acquisition_day):
     df = df.sort_values(['time_isoformat'], ignore_index=True)
 
     t = df['time_isoformat'].iloc[0]
-    print((min(df['time'])))
-    print((max(df['time'])))
 
     from datetime import timedelta
     time_delta = timedelta(minutes=time_delta)
@@ -97,8 +98,7 @@ def main(data, maxlevel, time_delta, acquisition_day):
         t = t + time_delta
 
     df['time_bin'] = pd.cut(df['time_isoformat'], bin_edges)
-    nan_index = df.loc[pd.isna(df["time_bin"]), :].index[0]
-    df = df.drop([nan_index], axis=0)
+    df = df.dropna()
     df.index = range(len(df))
 
     # computing mid_price_delta
@@ -114,7 +114,7 @@ def main(data, maxlevel, time_delta, acquisition_day):
         grouped = df[df['time_bin']==b]
         mid_price_delta.append(grouped['mid_price'].iloc[-1] - grouped['mid_price'].iloc[0])
         for l in range(maxlevel):
-            ofi.iloc[index, l+2]= grouped['e_{}'.format(l)].sum()
+            ofi.iloc[index, l+2]=grouped['e_{}'.format(l)].sum()
         index += 1
     ofi['mid_price_delta'] = mid_price_delta
 
